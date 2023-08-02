c01cc5b83bbdfe7f69dbb13ad9c50fdcbb79b19e
Patrick <pkcs@gft.com>
2023-08-02 17:45:26 -0300

    add project structures

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ailos-challenge';
+  checkIfDocumentIsValid: boolean | undefined = undefined;
+  checkoutForm = this.formBuilder.group({
+    cpf: '',
+  });
+  stepper = {
+    current: 0,
+    list: [
+      { label: 'Início' },
+      { label: 'Documentos' },
+      { label: 'Dados cadastrais' },
+      { label: 'Admissão' },
+    ]
+  }
+
+  constructor(
+    private formBuilder: FormBuilder,
+  ) { }
+
+  validateCPF(cpf: string) {
+    cpf = cpf.replace(/\D/g, '');
+
+    if (cpf.length !== 11) {
+      return false;
+    }
+
+    if (/^(\d)\1{10}$/.test(cpf)) {
+      return false;
+    }
+
+    let sum = 0;
+    let remainder;
+
+    for (let i = 1; i <= 9; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(9, 10))) {
+      return false;
+    }
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++) {
+      sum += parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    }
+
+    remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) {
+      remainder = 0;
+    }
+
+    if (remainder !== parseInt(cpf.substring(10, 11))) {
+      return false;
+    }
+
+    return true;
+  }
+
+  onSubmit(): void {
+    const { cpf } = this.checkoutForm.value;
+    this.checkIfDocumentIsValid = this.validateCPF(cpf || '')
+  }
+
+  onChange(value: string): void {
+    if (value.length < 14) {
+      this.checkIfDocumentIsValid = undefined;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { ButtonComponent } from './component/button/button.component';
+import { CardComponent } from './component/card/card.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { ToolbarComponent } from './component/toolbar/toolbar.component';
+import { SidebarComponent } from './component/sidebar/sidebar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { InfoContainerComponent } from './component/info-container/info-container.component';
+import { StepperComponent } from './component/stepper/stepper.component';
+import { DescriptionContainerComponent } from './component/description-container/description-container.component';
+import { CpfMaskDirective } from './app.directive';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CpfMaskDirective,
+    ToolbarComponent,
+    SidebarComponent,
   ],
   imports: [
+    ButtonComponent,
+    CardComponent,
+    FooterComponent,
+    InfoContainerComponent,
+    StepperComponent,
+    DescriptionContainerComponent,
+
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
+  exports: [CpfMaskDirective],
   providers: [],
   bootstrap: [AppComponent]
 })
