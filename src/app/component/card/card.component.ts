ecb5fd1c422f59f00c71a853d1725747d094dd49
Patrick <pkcs@gft.com>
2023-08-02 17:12:43 -0300

    create card component

---
+++ src/app/component/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-card',
+  standalone: true,
+  imports: [CommonModule, MatCardModule],
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss']
+})
+export class CardComponent {
+
+}
